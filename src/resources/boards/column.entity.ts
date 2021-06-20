a6804fa6f7d61d54519c7321ac7e116d1eaee5d4
Uporold <crubick1337@gmail.com>
2021-06-20 17:22:58 +0300

    feat: add entities WIP

---
+++ src/resources/boards/column.entity.ts
@@ -0,0 +1,33 @@
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { TaskEntity } from '../tasks/task.entity';
+import { BoardEntity } from './board.entity';
+
+@Entity('columns')
+export class ColumnEntity extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  order!: number;
+
+  @OneToMany((_type) => TaskEntity, (task) => task.column, {
+    eager: true,
+    cascade: true,
+  })
+  tasks!: TaskEntity[];
+
+  @ManyToOne((_type) => BoardEntity, (board) => board.columns, {
+    onDelete: 'CASCADE',
+  })
+  board!: BoardEntity;
+}
---
+++ src/resources/users/user.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { classToPlain, Exclude } from 'class-transformer';
+import { TaskEntity } from '../tasks/task.entity';
+
+@Entity('users')
+export class UserEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  login!: string;
+
+  @Exclude({ toPlainOnly: true })
+  @Column()
+  password!: string;
+
+  @OneToMany((_type) => TaskEntity, (task) => task.user, {
+    eager: false,
+  })
+  tasks!: TaskEntity[];
+
+  toJSON() {
+    return classToPlain(this);
+  }
+}
