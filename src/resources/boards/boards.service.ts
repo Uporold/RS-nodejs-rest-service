f182640355c1586d3f0a0ca2e995481d340fc663
Uporold <crubick1337@gmail.com>
2021-06-20 21:54:00 +0300

    refactor: remove comments

--- src/resources/boards/boards.service.ts
+++ src/resources/boards/boards.service.ts
@@ -2,17 +2,14 @@ import { StatusCodes } from 'http-status-codes';
 import { getConnection } from 'typeorm';
 import { BoardRepository } from './board.repository';
 import { CustomError } from '../../middlewares/error';
-// import { TasksService } from '../tasks/tasks.service';
 import { BoardDto } from './board.dto';
 import { BoardEntity } from './board.entity';
 
 export class BoardsService {
   private boardRepository: BoardRepository;
-  // private tasksService: TasksService;
 
   constructor() {
     this.boardRepository = getConnection().getCustomRepository(BoardRepository);
-    // this.tasksService = new TasksService();
   }
 
   async getAll(): Promise<BoardEntity[]> {
@@ -44,7 +41,6 @@ export class BoardsService {
   }
 
   async deleteBoard(id: string): Promise<void> {
-    // await this.getById(id);
     const result = await this.boardRepository.delete(id);
     if (result.affected === 0) {
       throw new CustomError(
@@ -52,6 +48,5 @@ export class BoardsService {
         `Board with id ${id} not found`
       );
     }
-    // await this.tasksService.removeByBoard(id);
   }
 }
