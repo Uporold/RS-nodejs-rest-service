397a0db1f9ede2e41c073952160db743615a85ee
Uporold <crubick1337@gmail.com>
2021-06-27 16:17:22 +0300

    feat: user create method with password hashing and login uniqueness check

--- src/resources/users/users.service.ts
+++ src/resources/users/users.service.ts
@@ -1,5 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 import { getConnection } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 import { UserRepository } from './user.repository';
 import { CustomError } from '../../middlewares/error';
 import { UserDto } from './user.dto';
@@ -17,8 +18,24 @@ export class UsersService {
   }
 
   async create(userDto: UserDto): Promise<UserEntity> {
-    const user = this.userRepository.create(userDto);
-    await this.userRepository.save(user);
+    const { login, name, password } = userDto;
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.userRepository.create({
+      login,
+      name,
+      password: hashedPassword,
+    });
+    try {
+      await this.userRepository.save(user);
+    } catch (err) {
+      if (err.code === '23505') {
+        throw new CustomError(
+          StatusCodes.CONFLICT,
+          'User with this login already exists'
+        );
+      }
+    }
     return user;
   }
 
